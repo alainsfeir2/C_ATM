859ee762f36ae60c32f5012e1325f050ea4ac746
alainsfeir2 <alainsfeir2899@gmail.com>
2022-01-20 10:53:33 -0500

    create login header file

---
+++ Lib/Login.h
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std ;
+
+bool sign_in(bool access){
+        string username;
+        string password;
+        cout<<"Enter username:"<< endl;
+        cin>>username;
+        cout<<"Enter password:"<< endl;
+        cin>>password;
+        //Login verification
+        string user_info[100][100];
+
+        ifstream users ( "Users/users.txt" );
+
+        if (!users)
+        {
+            cout << "There was an error opening the file.\n";
+            return 0;
+        }
+        else{
+            string line;
+            int row = 0;
+            int col = 0;
+
+            if (users.is_open())
+            {
+
+                while ( getline (users,line) ){
+                        int i=0;
+                        while (line[i] != ':'){
+                            string word = user_info[row][col];
+                            word= word + line[i];
+                            user_info[row][col]=word ;
+                            i = i+1;
+                            if (line[i] == ':'){
+                                
+                                col++;
+                                i++;
+                                
+                            }
+                            if (line[i] == '#'){
+                                row++;
+                                col=0;
+                                break;
+                            }
+                        }
+                            
+                    }
+                    users.close();
+            }
+            bool found;
+            string comp_pass;
+            string balance;
+            for(int i=0;i<100;i++){
+                
+                if(user_info[i][0] == username){
+                    comp_pass = user_info[i][1];
+                    balance = user_info[i][2];
+                    found = true;
+                    break;
+                    }
+                }
+            if (found == false){
+                cout<<"Username not found!"<<endl;
+                return access = false;
+            }else if(comp_pass != password){
+                cout<<"Password wrong!"<<endl;
+                return access = false;
+            }else{
+                return access = true;
+            }   
+
+        }
+}
