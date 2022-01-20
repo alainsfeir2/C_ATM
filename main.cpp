226a5a21652f24f6e4dc5d007d97315d25fb320a
alainsfeir2 <alainsfeir2899@gmail.com>
2022-01-20 10:55:19 -0500

    x

--- main.cpp
+++ main.cpp
@@ -3,7 +3,10 @@
 #include <string>
 #include <fstream>
 
+#include <F:\Programming\Coding\C++\Atm\C_ATM\Lib\Login.h>
+
 using namespace std ;
+
 
 int check_balance(string user);
 void add_balance(string user);
@@ -14,76 +17,6 @@ void sign_in(string user);
 int main() {
     bool access = false;
     while (!access){
-
-        string username;
-        string password;
-        cout<<"Enter username:"<< endl;
-        cin>>username;
-        cout<<"Enter password:"<< endl;
-        cin>>password;
-        //Login verification
-        string user_info[100][100];
-
-        ifstream users ( "Users/users.txt" );
-
-        if (!users)
-        {
-            cout << "There was an error opening the file.\n";
-            return 0;
-        }
-        else{
-            string line;
-            int row = 0;
-            int col = 0;
-
-            if (users.is_open())
-            {
-
-                while ( getline (users,line) ){
-                        int i=0;
-                        while (line[i] != ':'){
-                            string word = user_info[row][col];
-                            word= word + line[i];
-                            user_info[row][col]=word ;
-                            i = i+1;
-                            if (line[i] == ':'){
-                                
-                                col++;
-                                i++;
-                                
-                            }
-                            if (line[i] == '#'){
-                                row++;
-                                col=0;
-                                break;
-                            }
-                        }
-                            
-                    }
-                    users.close();
-            }
-            bool found;
-            string comp_pass;
-            string balance;
-            for(int i=0;i<100;i++){
-                
-                if(user_info[i][0] == username){
-                    comp_pass = user_info[i][1];
-                    balance = user_info[i][2];
-                    found = true;
-                    break;
-                    }
-                }
-            if (found == false){
-                cout<<"Username not found!"<<endl;
-                access = false;
-            }else if(comp_pass != password){
-                cout<<"Password wrong!"<<endl;
-                access = false;
-            }else{
-                access = true;
-            }   
-
-        }
+        access = sign_in(access);
     }
 }
